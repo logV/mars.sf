bfbdb479e4810f924dbcc4cf25d96300349f47c6
okay <okay.zed@gmail.com>
2013-11-30 00:09:01 -0800

    update route handlers to take ctx + API

--- superfluous/core/server/routes.js
+++ superfluous/core/server/routes.js
@@ -22,6 +22,26 @@ var install_socket = function(io) {
   socket.install(io, Controllers);
 };
 
+var template = require_core("server/template");
+var bridge = require_core("server/bridge");
+var readfile = require_core("server/readfile");
+
+var API = {
+  bridge: bridge,
+  page: page,
+  template: template,
+  readfile: readfile,
+  inspect: function() {
+    return "API: " + _.keys(API).sort().join(", ");
+  }
+};
+
+_.each(API, function(v, k) {
+  v.inspect = function() {
+    return k + ":" + _.keys(v).sort().join(",");
+  };
+});
+
 function request_handler_factory(route, handler) {
   return function handle_req(req, res) {
     context.create({ req: req, res: res }, function(ctx) {
@@ -32,7 +52,8 @@ function request_handler_factory(route, handler) {
 
       debug("Starting request", ctx.id, ctx.req.url);
       res.set("Transfer-Encoding", "chunked");
-      handler();
+
+      handler(ctx, API);
 
       page.emit("main_duration");
       debug("Ending main request", ctx.id, ctx.req.url);
